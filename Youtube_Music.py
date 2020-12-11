a670a8447f34b335fae96ce7d7bb133f3e50dfc3
ngbjr <35680421+ngbjr@users.noreply.github.com>
2020-12-11 21:15:12 +0800

    Add files via upload

---
+++ Youtube_Music.py
@@ -0,0 +1,42 @@
+import threading, time, pafy, vlc, urllib.request, re
+
+playlist = []
+
+def get_sec(time_str):
+    h, m, s= time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+def youtube_search(search):
+    c_result = search.replace(" ", "+")
+    get_url = "https://www.youtube.com/results?search_query=" + c_result
+    get_html = urllib.request.urlopen(get_url)
+    video_ids = re.findall(r"watch\?v=(\S{11})", get_html.read().decode())
+    result = pafy.new("https://www.youtube.com/watch?v=" + video_ids[0])
+    return result
+
+
+def insert():
+    playlist.append(input("Search Music:"))
+
+def start():
+    global playlist
+
+    if len(playlist) == 0:
+        playlist.append(input("Search Music:"))
+        start()
+
+    for x in playlist:
+        music = youtube_search(x)
+        s_duration = get_sec(music.duration)
+        audio = music.getbestaudio()
+        audio_url = audio.url
+        player = vlc.MediaPlayer(audio_url)
+        player.audio_set_volume(40)
+        player.play()
+        while s_duration > 0:
+            s_duration -=1
+            t1 = threading.Thread(target=insert)
+            t1.start()
+            time.sleep(1)
+
+start()
